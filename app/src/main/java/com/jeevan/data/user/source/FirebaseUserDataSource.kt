62e6138b67c5a75f7177e32c58ef2b30b045efcb
Jeevan Surendran <jvns67@gmail.com>
2021-06-20 15:44:33 +0530

    Add firebase flows

---
+++ app/src/main/java/com/jeevan/data/user/source/FirebaseUserDataSource.kt
@@ -0,0 +1,31 @@
+package com.jeevan.data.user.source
+
+import com.google.firebase.auth.FirebaseAuth
+import com.jeevan.data.user.model.FirebaseUserInfo
+import kotlinx.coroutines.channels.ClosedSendChannelException
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.channels.onClosed
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.isActive
+import javax.inject.Inject
+
+class FirebaseUserDataSource @Inject constructor(val auth: FirebaseAuth) {
+    fun getObservableFirebaseUser(): Flow<FirebaseUserInfo> {
+        return channelFlow {
+            val authStateListener: ((FirebaseAuth) -> Unit) = { auth ->
+                if (isActive) {
+                    auth.currentUser.let { it ->
+                        channel.trySend(FirebaseUserInfo(it)).onClosed {
+                            throw it ?: ClosedSendChannelException("Channel was closed normally")
+                        }.isSuccess
+                    }
+                }
+            }
+            auth.addAuthStateListener(authStateListener)
+            awaitClose {
+                auth.removeAuthStateListener(authStateListener)
+            }
+        }
+    }
+}
