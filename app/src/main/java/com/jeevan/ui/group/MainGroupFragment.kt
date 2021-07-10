225510940da84f959c5e78d9dc27c18da75ab383
Jeevan Surendran <jvns67@gmail.com>
2021-07-10 22:01:29 +0530

    Add GroupFragment

---
+++ app/src/main/java/com/jeevan/ui/group/MainGroupFragment.kt
@@ -0,0 +1,12 @@
+package com.jeevan.ui.group
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.jeevan.R
+
+class MainGroupFragment: Fragment(R.layout.fragment_main_group) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    }
+}
